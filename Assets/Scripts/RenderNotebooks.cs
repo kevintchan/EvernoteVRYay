ba5df54903416141c272506f6a7dd12a3e872195
Kevin Chan <kchan@evernote.com>
2017-01-09 18:32:10 -0800

    Add evernote C# SDK.

---
+++ Assets/Scripts/RenderNotebooks.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Evernote.EDAM.Type;
+
+public class RenderNotebooks : MonoBehaviour {
+
+	public GameObject prefabNotebook;
+
+	// Use this for initialization
+	// change this to occur on a trigger
+
+	void Awake () {
+		Notebook[] nbs = { new Notebook() };
+		nbs [0].Name = "Leo Gong";
+			
+		foreach (Notebook n in nbs) {
+			Instantiate(prefabNotebook, new Vector3(i * 2.0f, 0, 4), Quaternion.identity);
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
